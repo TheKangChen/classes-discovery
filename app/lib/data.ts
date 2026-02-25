75c1a3d4392b968f0e4b300b2485b691991b361d
TheKangChen <kangchen.work@gmail.com>
2026-02-25 08:19:20 -0500

    update data fetching function to match all filter & handle initial no query state

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -6,10 +6,10 @@ import { Pinecone } from '@pinecone-database/pinecone';
 import { ClassResult } from "@/app/lib/definitions";
 
 
-const hf = new InferenceClient(process.env.HF_TOKEN);
-const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY });
+const hf = new InferenceClient(process.env.HF_TOKEN!);
+const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY! });
 const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
@@ -25,41 +25,57 @@ const supabase = createClient(
 export async function getDiscoveryResult(
     query: string,
     selectedLevels: string[],
+    selectedFormats: string[],
     selectedSeries: string[]
 ): Promise<ClassResult[]> {
+    console.log(selectedLevels);
+    console.log(selectedFormats);
+    console.log(selectedSeries);
     try {
         // Semantic search
-        const queryEmbedding = await hf.featureExtraction({
-            model: "BAAI/bge-small-en-v1.5:fastest",
-            inputs: query || 'technology',
-            provider: "auto",
-        }) as number[];
-        const index = pc.index(process.env.PINECONE_INDEX, process.env.PINECONE_INDEX_HOST);
-        const vectorResults = await index.query({
-            vector: queryEmbedding,
-            topK: 50,
-            includeMetadata: false
-        });
-        const semanticIds = vectorResults.matches.map(m => m.id);
+        if (query) {
+            const queryEmbedding = await hf.featureExtraction({
+                model: "BAAI/bge-small-en-v1.5",
+                inputs: query || 'technology',
+                provider: "hf-inference",
+            }) as number[];
+            const index = pc.index(process.env.PINECONE_INDEX!, process.env.PINECONE_INDEX_HOST!);
+            const vectorResults = await index.query({
+                vector: queryEmbedding,
+                topK: 50,
+                includeMetadata: false
+            });
+            const semanticIds = vectorResults.matches.map(m => m.id);
 
-        // Lexical search filtered with semantic search results
-        let pgQuery = supabase
-            .from('classes')
-            .select('id, class_title, description, prerequisite, level, series, format')
-            .in('id', semanticIds);
-        if (selectedLevels.length > 0) {
-            pgQuery.in('level', selectedLevels);
+            // Lexical search filtered with semantic search results
+            let pgQuery = supabase
+                .from('classes')
+                .select('id, class_title, description, prerequisite, level, series, format')
+                .in('id', semanticIds);
+
+            if (selectedLevels.length > 0) pgQuery.in('level', selectedLevels);
+            if (selectedFormats.length > 0) pgQuery.in('format', selectedFormats);
+            if (selectedSeries.length > 0) pgQuery.overlaps('series', selectedSeries);
+            const { data: filteredItems, error: pgError } = await pgQuery
+            if (pgError) throw pgError;
+
+            // Sort on semantic relavance
+            const results = filteredItems.sort((a, b) => semanticIds.indexOf(a.id) - semanticIds.indexOf(b.id));
+
+            return results;
+        } else {
+            let pgQuery = supabase
+                .from('classes')
+                .select('id, class_title, description, prerequisite, level, series, format')
+
+            if (selectedLevels.length > 0) pgQuery.in('level', selectedLevels);
+            if (selectedFormats.length > 0) pgQuery.in('format', selectedFormats);
+            if (selectedSeries.length > 0) pgQuery.overlaps('series', selectedSeries);
+            const { data: filteredItems, error: pgError } = await pgQuery
+            if (pgError) throw pgError;
+
+            return filteredItems;
         }
-        if (selectedSeries.length > 0) {
-            pgQuery.in('series', selectedSeries);
-        }
-        const { data: filteredItems, error: pgError } = await pgQuery
-        if (pgError) throw pgError;
-
-        // Sort on semantic relavance
-        const results = filteredItems.sort((a, b) => semanticIds.indexOf(a.id) - semanticIds.indexOf(b.id));
-
-        return results;
     } catch (error) {
         console.log("Search Error:", error);
         return [];
