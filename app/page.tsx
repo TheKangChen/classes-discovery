6e2745c06b6a86ccf5faef75d47f496af72567a2
TheKangChen <kangchen.work@gmail.com>
2026-02-25 08:28:33 -0500

    created discovery page

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,57 @@
-import Image from "next/image";
 import Search from "@/app/ui/search";
 import { Card } from "@/app/ui/cards";
-import { Suspense } from "react";
+import { getDiscoveryResult } from "./lib/data";
 
-export default function Page() {
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+        level?: string;
+        format?: string;
+        series?: string;
+    }>;
+}) {
+    const searchParams = await props.searchParams;
+
+    const query = searchParams?.query || '';
+    const levels = searchParams?.level ? searchParams.level.split(',') : [];
+    const formats = searchParams?.format ? searchParams.format.split(',') : [];
+    const series = searchParams?.series ? searchParams.series.split(',') : [];
+
+    const results = await getDiscoveryResult(query, levels, formats, series);
+    const totalResults = results.length;
+
+    console.log(results);
+
     return (
-        <main className="">
+        <main className="max-w-5xl mx-auto px-1 py-8">
             <div className="">
-                <div className="">
-                    <div>
-                        <h1 className="">
-                            Technology Classes Discovery
-                        </h1>
-                    </div>
-                    <div className="">
-                        <Search placeholder="Search classes" />
-                    </div>
+                <div className="border-b border-gray-300">
+                    <h1 className="text-3xl font-bold mb-6">Classes Discovery</h1>
+                    <Search placeholder="Search classes" />
                 </div>
-                <br />
-                <hr />
-                <br />
-                <div>
-                    <Card
-                        title="Fundamentals of programming with Python part 1"
-                        description="Sed quis esse aut neque. Commodi fugiat maxime corrupti. Inventore amet quia rerum vero esse a voluptatem. Quis error voluptatem consequatur iusto quasi sit. Et quam optio atque ut incidunt enim asperiores quis. Excepturi excepturi cupiditate cupiditate officiis eius non."
-                        level="Intermediate"
-                    />
+                <div className="mt-8 flex items-baseline justify-between border-b border-gray-200 pb-2">
+                    <p className="text-sm text-gray-600">
+                        Showing <span className="font-bold">1</span> through <span className="font-bold">{totalResults}</span> of <span className="font-bold">{totalResults}</span>
+                        {query && (
+                            <span> for the search: <span className="italic text-blue-700">"{query}"</span></span>
+                        )}
+                    </p>
+                </div>
+                <div className="flex flex-col">
+                    {results.length > 0 ? (
+                        results.map(item => (
+                            <Card
+                                key={item.id}
+                                title={item.class_title}
+                                description={item.description}
+                                level={item.level}
+                            />
+                        ))
+                    ) : (
+                        <div className="py-20 text-center">
+                            <p className="text-gray-500 italic">No classes found</p>
+                        </div>
+                    )}
                 </div>
             </div>
         </main>
